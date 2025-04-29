ffd8a717788d263eaab92c38d2b79c02aca87d61
asliparas007 <paraschawla040@gmail.com>
2025-04-30 01:00:59 +0530

    report

--- tests/test_dashboard.py
+++ tests/test_dashboard.py
@@ -18,4 +18,20 @@ def test_pim_add_employee(valid_user):
 
 
 
+def test_add_report(valid_user):
+    driver = valid_user
+    dashboard = Dashboard(driver)
+    dashboard.get_pim_left_menu()
+    driver.find_element(By.XPATH,"//a[text()='Reports']").click()
+    driver.find_element(By.XPATH,"//button[text()=' Add ']").click()
+    driver.find_element(By.XPATH,"//input[@placeholder='Type here ...']").send_keys("Test Report")
+    dropdowns = driver.find_elements(By.CLASS_NAME,"oxd-icon.bi-caret-down-fill.oxd-select-text--arrow")
+    selection_cretria = dropdowns[0]
+    selection_cretria.click()
+    dropdown_options = driver.find_elements(By.CLASS_NAME, 'oxd-select-option')
+    for item in dropdown_options:
+        if item.text == "Pay Grade":
+            item.click()
+            break
+
     time.sleep(5)
